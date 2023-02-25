12a1df81dc060ebed2d613977caafed946a3c31a
BabuBubble <xiangtailangxize925@gmail.com>
2023-02-25 11:44:45 +0900

    alert.dartの目標支出を保存できるようにした

--- lib/pages/alert.dart
+++ lib/pages/alert.dart
@@ -1,19 +1,33 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class Alert {
   final List<String> Emoticon = ['😆', '😀', '🙂', '😥', '😱'];
-  final int targetSpend = 30000; //目標支出
-  int rtnState(int spend) {
+
+  Future setTargetSpend(int targetSpend) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('targetSpend', targetSpend);
+  }
+
+  Future<int> getTargetSpend() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    int rtn = await prefs.getInt('targetSpend') ?? 0;
+    return rtn;
+  }
+
+  Future<int> rtnState(int spend) async {
     int subscript;
+    int targetSpend = await getTargetSpend(); //目標支出
 
     for (subscript = 1; subscript < 5; subscript++) {
       if (spend < subscript * targetSpend * 2 / 5) {
         break;
       }
     }
-    return subscript - 1;
+    return Future.value(subscript - 1);
   }
 
-  String alert(int spend) {
+  Future<String> alert(int spend) async {
     //状態に応じた顔文字を返す
-    return Emoticon[rtnState(spend)];
+    return Future.value(Emoticon[await rtnState(spend)]);
   }
 }
